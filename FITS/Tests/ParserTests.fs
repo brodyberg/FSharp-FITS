923c172bb71233278c7c729a23dc32371faeaa82
Brody Berg <brodyberg@gmail.com>
2015-06-06 16:59:13 -0700

    Organize Tests more realistically

---
+++ FITS/Tests/ParserTests.fs
@@ -0,0 +1,24 @@
+﻿namespace Tests.FITS
+
+open Xunit 
+open Swensen.Unquote
+
+module Parser = 
+
+    [<Fact>]
+    let ``When we add we get three`` () = 
+        test <@ (1+2)/3 = 1 @>
+
+    [<Fact>]
+    let ``When we add we get two`` () = 
+        test <@ (1+2)/3 = 2 @>
+
+
+
+
+
+
+
+
+
+
