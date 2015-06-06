c765e8da402ad4d45b759525fde6d05b1abaf395
Brody Berg <brodyberg@gmail.com>
2015-06-06 16:57:27 -0700

    Start solution, with Unquote/Xunit tests

---
+++ FITS/FITS/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace FITS
+
+type Class1() = 
+    member this.X = "F#"
---
+++ FITS/Tests/Script.fsx
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+//#load "Library1.fs"
+//open Tests
+
+// Define your library scripting code here
+
+#r @"../packages/Unquote.3.0.0/lib/net45/Unquote.dll"
+
+open Swensen.Unquote
+
+test <@ (1+2)/3 = 1 @>
+
+test <@ (1 + 2) = 2 @> 
+
+
+
+
+
+
+
+
+
+
+
